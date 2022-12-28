2bc3feb072908660016614797b612fdc882c26be
user <3522003+cdaein@users.noreply.github.com>
2022-12-28 18:02:10 -0500

    update demo

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,22 +1,39 @@
-import { createCanvas } from "../index";
+import { createCanvas, resizeCanvas } from "../index";
 
 const parent = document.createElement("div");
 parent.id = "app";
 document.body.appendChild(parent);
 
-const canvas = createCanvas({
+let canvas = createCanvas({
   parent,
   width: window.innerWidth,
   height: window.innerHeight,
-  pixelRatio: 2,
+  pixelRatio: window.devicePixelRatio,
 });
-const ctx = canvas.getContext("2d")!;
 const { width, height } = canvas;
+const ctx = canvas.getContext("2d")!;
 
-ctx.fillStyle = `gray`;
-ctx.fillRect(0, 0, width, height);
+const draw = (width: number, height: number) => {
+  ctx.fillStyle = `gray`;
+  ctx.fillRect(0, 0, width, height);
 
-ctx.beginPath();
-ctx.arc(width / 2, height / 2, width / 4, 0, Math.PI * 2);
-ctx.fillStyle = `white`;
-ctx.fill();
+  ctx.beginPath();
+  ctx.arc(width / 2, height / 2, width / 4, 0, Math.PI * 2);
+  ctx.fillStyle = `white`;
+  ctx.fill();
+};
+
+draw(width, height);
+
+// respond to resize event
+window.addEventListener("resize", () => {
+  canvas = resizeCanvas({
+    canvas,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    pixelRatio: window.devicePixelRatio,
+  });
+  const { width, height } = canvas;
+
+  draw(width, height);
+});
