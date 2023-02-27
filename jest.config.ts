7ea1885aaacb8d391db73a38fb9ed6339db8995e
user <3522003+cdaein@users.noreply.github.com>
2023-02-26 22:32:55 -0500

    remove dist from git

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-
-module.exports = {
-  preset: "ts-jest",
-  setupFiles: ["jest-canvas-mock"],
-  testEnvironment: "jest-environment-jsdom",
-  // transform: {},
-};
